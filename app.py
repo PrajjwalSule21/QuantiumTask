eafd71d640a53d9597d0a0b752d4fb13c7b59111
PrajjwalSule21 <suleprajjwal21@gmail.com>
2022-06-13 17:48:32 +0530

    Visialize the Sales thrid task

---
+++ app.py
@@ -0,0 +1,35 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H2('Analysing Sales of Pink Morsel'),
+    dcc.Graph(id="graph"),
+    dcc.Checklist(
+        id="checklist",
+        value=["north", "south", "east","west"],
+        inline=True
+    ),
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("checklist", "value"))
+
+
+def update_line_chart(sales):
+    df = pd.read_csv('output.csv') # replace with your own data source
+    df['Date'] = pd.to_datetime(df['Date'])
+    # mask = df.continent.isin(continents)
+    fig = px.line(df, x="Date", y="Sales", height=600)
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
--- main.py
+++
@@ -1,8 +0,0 @@
-# This is a sample Python script.
-def task(name):
-    print(f'This is a {name} task')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    task('Quantium')
-
