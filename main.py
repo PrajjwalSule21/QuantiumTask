a5a3695066a163ececae2fd35f18ba0ed9f44d62
PrajjwalSule21 <suleprajjwal21@gmail.com>
2022-06-13 14:19:36 +0530

    Env-Setup Intial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+def task(name):
+    print(f'This is a {name} task')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    task('Quantium')
+
